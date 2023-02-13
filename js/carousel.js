d699b88bd5d4647588146eb577dcd529e496eeb9
Saori Tsutsumi <78334912+saya4017@users.noreply.github.com>
2023-02-13 15:12:51 +0900

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        // appendDots: ,
+        // centerMode: true, // コメントアウトする
+        dots: true,
+        // fade: true,
+        // mobileFirst: ,
+        arrows: false, // 追加する
+    });
+});
